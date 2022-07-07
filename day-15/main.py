86db744bacac24a824f73fd90a06351dbe2379d3
O_ <104556158+Bishamonka@users.noreply.github.com>
2022-07-07 21:58:04 +0300

    Update main.py

--- day-15/main.py
+++ day-15/main.py
@@ -1,3 +1,4 @@
 print("Hello word!")
 print("Finally! We are moving to Py Charm. I can`t wait to start building stuff in here!")
 print("I've missed yesteeday's contribution... This is gonna drive me crazy for a whole year( ")
+print("2 Days")
