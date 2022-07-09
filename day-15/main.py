1595b9d1caa7a9f516644db0735ddc155709ab5e
O_ <104556158+Bishamonka@users.noreply.github.com>
2022-07-09 22:32:11 +0300

    Update main.py

--- day-15/main.py
+++ day-15/main.py
@@ -2,6 +2,7 @@ print("Hello word!")
 print("Finally! We are moving to Py Charm. I can`t wait to start building stuff in here!")
 print("I've missed yesteeday's contribution... This is gonna drive me crazy for a whole year( ")
 print("2 Days")
+print("More than that...")
 
 MENU = {
     'espresso': {
